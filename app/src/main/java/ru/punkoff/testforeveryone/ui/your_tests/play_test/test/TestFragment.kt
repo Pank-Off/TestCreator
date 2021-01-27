6d9cb230e0d18e177dc43a2a5daf1308a357b750
Punk_Off <pankov.kirill1997@yandex.ru>
2021-01-28 00:03:58 +0300

    TestFragment redesign

--- app/src/main/java/ru/punkoff/testforeveryone/ui/your_tests/play_test/test/TestFragment.kt
+++ app/src/main/java/ru/punkoff/testforeveryone/ui/your_tests/play_test/test/TestFragment.kt
@@ -7,9 +7,11 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.button.MaterialButton
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import org.koin.core.parameter.parametersOf
 import ru.punkoff.testforeveryone.MainActivity
+import ru.punkoff.testforeveryone.R
 import ru.punkoff.testforeveryone.data.TempResult
 import ru.punkoff.testforeveryone.data.local.room.TestEntity
 import ru.punkoff.testforeveryone.data.local.room.mapToColor
@@ -20,6 +22,8 @@ class TestFragment : Fragment() {
 
     private val adapter = TestAdapter()
 
+    private var position = 0
+    private var maxSize = 0
     private val test: TestEntity? by lazy(LazyThreadSafetyMode.NONE) {
         arguments?.getParcelable(
             EXTRA_TEST
@@ -43,34 +47,58 @@ class TestFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         test?.color?.mapToColor()?.let { view.setBackgroundResource(it) }
         testViewModel.clearScore()
+        adapter.firstStart = true
+        position = 0
         Log.d(javaClass.simpleName, "Test + $test")
-        adapter.attachListener { answer, score ->
-            Log.d(javaClass.simpleName, "answer: $answer score $score")
-            score?.toInt()?.let { testViewModel.refreshScore(answer, it) }
-        }
         with(binding) {
+            adapter.attachListener { answer, score ->
+                Log.d(javaClass.simpleName, "answer: $answer score $score")
+                score?.toInt()?.let {
+                    testViewModel.refreshScore(answer, it)
+                    if (position == maxSize) {
+                        showResultBtn.text = getString(R.string.show_results)
+                        showResultBtn.visibility = MaterialButton.VISIBLE
+                    }
+                    if (position < maxSize) {
+                        Log.d(javaClass.simpleName, "Position in TestFragment $position")
+                        adapter.showQuestion(position++)
+                    }
+                }
+            }
+
             questionList.adapter = adapter
             questionList.layoutManager = LinearLayoutManager(context)
             titleView.text = test?.title
             showResultBtn.setOnClickListener {
-                var score = 0
-                testViewModel.getScore().value?.forEach {
-                    Log.d("getScore: ", it.value.toString())
-                    score += it.value
-                }
-                test?.let { it1 -> testViewModel.createResult(it1, score) }
-                Log.d(javaClass.simpleName, "Score: $score ")
-                testViewModel.getResultLiveData().observe(viewLifecycleOwner) {
-                    Log.d(javaClass.simpleName, "getResultLiveData: ${it.getResult()}")
-                    navigateToShowResultFragment(it)
-                }
+                showResultBtn.visibility = MaterialButton.GONE
 
+                adapter.showQuestion(position++)
+                Log.d(javaClass.simpleName, "Position: $position vs. MaxSize: $maxSize")
+                if (position - 1 == maxSize) {
+                    showResult()
+                }
             }
         }
+
 
         testViewModel.observeViewState().observe(viewLifecycleOwner) {
             Log.d(javaClass.simpleName, "questions: ${it?.questions}")
             adapter.submitList(it?.questions)
+            maxSize = it?.questions?.size!!
+        }
+    }
+
+    private fun showResult() {
+        var score = 0
+        testViewModel.getScore().value?.forEach {
+            Log.d("getScore: ", it.value.toString())
+            score += it.value
+        }
+        test?.let { it1 -> testViewModel.createResult(it1, score) }
+        Log.d(javaClass.simpleName, "Score: $score ")
+        testViewModel.getResultLiveData().observe(viewLifecycleOwner) {
+            Log.d(javaClass.simpleName, "getResultLiveData: ${it.getResult()}")
+            navigateToShowResultFragment(it)
         }
     }
 
